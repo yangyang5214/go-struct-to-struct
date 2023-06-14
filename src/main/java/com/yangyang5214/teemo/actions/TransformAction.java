911b8e4bafb7119401aa77bce2c2ad8197b72b01
beer <beer5214@126.com>
2023-06-14 21:08:54 +0800

    set text action success

--- src/main/java/com/yangyang5214/teemo/actions/TransformAction.java
+++ src/main/java/com/yangyang5214/teemo/actions/TransformAction.java
@@ -1,20 +1,22 @@
 package com.yangyang5214.teemo.actions;
 
-import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
 public class TransformAction extends AnAction {
 
 
     @Override
-    public void actionPerformed(@NotNull AnActionEvent event) {
-        Project project = event.getProject();
-        String dlgMsg = event.getPresentation().getText() + " Selected!";
-        Messages.showMessageDialog(project, dlgMsg, "Test", Messages.getInformationIcon());
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        Project project = e.getProject();
+        Document doc = e.getRequiredData(CommonDataKeys.EDITOR).getDocument();
+        WriteCommandAction.runWriteCommandAction(project, () ->
+                doc.setText("beer test")
+        );
     }
-
 }
