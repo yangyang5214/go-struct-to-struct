eb1500578bd82706f2be922d1c2b935120bc9f64
beer <beer5214@126.com>
2023-06-15 14:03:27 +0800

    support insert text

--- src/main/java/com/yangyang5214/teemo/actions/TransformAction.java
+++ src/main/java/com/yangyang5214/teemo/actions/TransformAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.editor.*;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
@@ -16,15 +17,52 @@ public class TransformAction extends AnAction {
         Project project = e.getProject();
         Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
         Document doc = editor.getDocument();
-        WriteCommandAction.runWriteCommandAction(project, () -> {
-            // Get caretOffset
-            // https://plugins.jetbrains.com/docs/intellij/coordinates-system.html#caret-visual-position
-            CaretModel caretModel = editor.getCaretModel();
-            Caret primaryCaret = caretModel.getPrimaryCaret();
-            int caretOffset = primaryCaret.getOffset();
 
-            //insert
-            doc.insertString(caretOffset, "test");
+        String currentLine = getCurrentLine(editor, doc);
+
+        String result = genResult();
+        result = currentLine; //todo
+
+        String finalResult = result;
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            insertString(editor, doc, finalResult);
         });
+    }
+
+
+    public String genResult() {
+        //todo
+        return "";
+    }
+
+    public String getCurrentLine(Editor editor, Document doc) {
+        int line = doc.getLineNumber(getCurrentOffset(editor));
+        TextRange textRange = new TextRange(doc.getLineStartOffset(line), doc.getLineEndOffset(line));
+        return doc.getText(textRange);
+    }
+
+
+    /**
+     * <a href="https://plugins.jetbrains.com/docs/intellij/coordinates-system.html#caret-visual-position">...</a>
+     *
+     * @param editor
+     * @return offset
+     */
+    public int getCurrentOffset(Editor editor) {
+        CaretModel caretModel = editor.getCaretModel();
+        Caret primaryCaret = caretModel.getPrimaryCaret();
+        return primaryCaret.getOffset();
+    }
+
+
+    /**
+     * insert test
+     *
+     * @param editor
+     * @param doc
+     * @param result
+     */
+    public void insertString(Editor editor, Document doc, String result) {
+        doc.insertString(getCurrentOffset(editor), result);
     }
 }
