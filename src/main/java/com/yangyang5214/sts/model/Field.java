5af68f9ae14372268d246e2c6b6ff4a01642b580
beer <beer5214@126.com>
2023-07-05 13:38:15 +0800

    support not match field fill

---
+++ src/main/java/com/yangyang5214/sts/model/Field.java
@@ -0,0 +1,34 @@
+package com.yangyang5214.sts.model;
+
+public class Field {
+    String name;
+    String type;
+
+    public Field() {
+    }
+
+    public Field(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
+
+
+
