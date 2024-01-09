3b44bfa023ad1ea3c8d18f01b09caf7dda2d085b
beer <beer5214@126.com>
2024-01-09 20:18:25 +0800

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.14.1"
+    id("org.jetbrains.intellij") version "1.16.1"
 }
 
 group = "com.yangyang5214"
-version = "0.0.5"
+version = "0.0.6"
 
 repositories {
     mavenCentral()
@@ -21,8 +21,8 @@ intellij {
 //    version.set("2022.2.5")
 
     // local develop idea location
-    localPath.set("/Users/beer./Library/Application Support/JetBrains/Toolbox/apps/Goland/ch-0/222.4345.24/GoLand.app/Contents")
-//    localPath.set("/Users/beer/Library/Application Support/JetBrains/Toolbox/apps/Goland/ch-0/231.9011.34/GoLand.app/Contents")
+//    localPath.set("/Users/beer./Library/Application Support/JetBrains/Toolbox/apps/Goland/ch-0/222.4345.24/GoLand.app/Contents")
+    localPath.set("/Users/beer/Library/Application Support/JetBrains/Toolbox/apps/Goland/ch-0/233.13135.104/GoLand.app/Contents")
 
     // https://plugins.jetbrains.com/docs/intellij/goland.html#plugin-and-module-dependencies
     plugins.set(listOf("org.jetbrains.plugins.go"))
@@ -36,6 +36,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("232.*")
+        // https://plugins.jetbrains.com/docs/intellij/build-number-ranges.html#platformVersions
+        untilBuild.set("233.*")
     }
 }
